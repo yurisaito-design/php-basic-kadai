b8efa51bca9d828e111fc46a2f4ed48e8ee2fdbe
yurisaito-design <waaaaan576@gmail.com>
2025-03-17 02:22:10 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHPテスト</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $vegetables_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($vegetables_data);
+    ?>
+  </p>
+</body>
+</html>
